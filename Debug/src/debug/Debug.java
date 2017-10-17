4d7d671c133ca1bde34f424d962562a8bad5b826
alice-sinisi <sinisi.alice95@gmail.com>
2017-10-17 11:52:57 +0200

    Add files via upload

---
+++ Debug/src/debug/Debug.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package debug;
+
+import static java.lang.System.out;
+ /**
+ *
+ * @author alice
+ */
+public class Debug {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       out.println(Debug.debug());
+    }
+    
+     public static String debug(){
+        String pippo;
+        pippo="Errore";
+        return pippo;
+        }
+    
+}
---
+++ Debug/src/debugoff/Debugoff.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package debugoff;
+
+import static java.lang.System.out;
+ /**
+ *
+ * @author alice
+ */
+public class Debugoff {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Debugoff.debug();
+    }
+    
+     public static String debug(){
+        String pippo;
+        pippo="Errore";
+        return pippo;
+        }
+    
+}
