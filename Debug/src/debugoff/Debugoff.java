641504436cc977ec75f0939b78b22def955453af
alice-sinisi <sinisi.alice95@gmail.com>
2017-10-23 13:07:06 +0200

    Update Debugoff.java

--- Debug/src/debugoff/Debugoff.java
+++ Debug/src/debugoff/Debugoff.java
@@ -10,15 +10,8 @@ import static java.lang.System.out;
  *
  * @author alice
  */
-public class Debugoff {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-       Debugoff.debug();
-    }
-    
+public class Debugoff{
+ 
      public static String debug(){
         String pippo;
         pippo="Errore";
