7ab699544ee31d66c4abf0bdb2ef5cec08d8dbd8
alice-sinisi <sinisi.alice95@gmail.com>
2017-10-25 11:17:10 +0200

    Update Debug.java

--- Debug/src/debug/Debug.java
+++ Debug/src/debug/Debug.java
@@ -1,23 +1,20 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package debug;
-
-import debugoff.Debugoff;
-import static java.lang.System.out;
  /**
  *
  * @author alice
  */
-public class Debug {
+public class Debug{
 
-        public static String debug(){
+  
+     public String debug(){
         String pippo;
         pippo="Errore";
         return pippo;
         }
+     
+     public String stampaDebug(){
+         return "Esiste un "+debug();
+     }
     
 }
 
